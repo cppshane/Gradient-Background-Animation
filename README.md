f6edc966b36c7b9cd6eea40d7b9694803f73d6cf
Shane Duffy <shane@shaneduffy.io>
2020-03-19 13:05:14 -0700

    init commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Gradient Background Animation
+Application for Android showing how to create a "flowing" color effect.
+
+Information about this project can be found on the SO link the idea originated from:
+http://stackoverflow.com/questions/35824330/colors-flowing-in-android-background/35824997#35824997
---
+++ app/src/main/java/com/example/shane/gradientbackgroundanimation/MainActivity.java
@@ -0,0 +1,105 @@
+package com.example.shane.gradientbackgroundanimation;
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.TransitionDrawable;
+import android.os.Handler;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageView image = (ImageView) findViewById(R.id.background);
+
+        Drawable backgrounds[] = new Drawable[3];
+        backgrounds[0] = ContextCompat.getDrawable(this, R.drawable.gradient1);
+        backgrounds[1] = ContextCompat.getDrawable(this, R.drawable.gradient2);
+        backgrounds[2] = ContextCompat.getDrawable(this, R.drawable.gradient3);
+
+        Crossfade(image, backgrounds, 10000);
+    }
+
+    public void Crossfade(final ImageView image, final Drawable layers[], final int speedInMs) {
+        class BackgroundGradientThread implements Runnable {
+            Context mainContext;
+            TransitionDrawable crossFader;
+            boolean first = true;
+
+            BackgroundGradientThread(Context c) {
+                mainContext = c;
+            }
+
+            public void run() {
+                Handler mHandler = new Handler(mainContext.getMainLooper());
+                boolean reverse = false;
+
+                while (true) {
+                    if (!reverse) {
+                        for (int i = 0; i < layers.length - 1; i++) {
+                            Drawable tLayers[] = new Drawable[2];
+                            tLayers[0] = layers[i];
+                            tLayers[1] = layers[i + 1];
+
+                            final TransitionDrawable tCrossFader = new TransitionDrawable(tLayers);
+                            tCrossFader.setCrossFadeEnabled(true);
+
+                            Runnable transitionRunnable = new Runnable() {
+                                @Override
+                                public void run() {
+                                    image.setImageDrawable(tCrossFader);
+                                    tCrossFader.startTransition(speedInMs);
+                                }
+                            };
+
+                            mHandler.post(transitionRunnable);
+
+                            try {
+                                Thread.sleep(speedInMs);
+                            } catch (Exception e) {
+                            }
+                        }
+
+                        reverse = true;
+                    }
+                    else if (reverse) {
+                        for (int i = layers.length - 1; i > 0; i--) {
+                            Drawable tLayers[] = new Drawable[2];
+                            tLayers[0] = layers[i];
+                            tLayers[1] = layers[i - 1];
+
+                            final TransitionDrawable tCrossFader = new TransitionDrawable(tLayers);
+                            tCrossFader.setCrossFadeEnabled(true);
+
+                            Runnable transitionRunnable = new Runnable() {
+                                @Override
+                                public void run() {
+                                    image.setImageDrawable(tCrossFader);
+                                    tCrossFader.startTransition(speedInMs);
+                                }
+                            };
+
+                            mHandler.post(transitionRunnable);
+
+                            try {
+                                Thread.sleep(speedInMs);
+                            } catch (Exception e) {
+                            }
+                        }
+
+                        reverse = false;
+                    }
+                }
+            }
+        }
+
+        Thread backgroundThread = new Thread(new BackgroundGradientThread(this));
+        backgroundThread.start();
+    }
+}
